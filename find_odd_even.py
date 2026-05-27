09db8c93fed9115d64c33b01ebb1d56828183c2e
Snehasis Satapathy <snehasissatapathy2005@gmail.com>
2026-05-27 16:19:07 +0530

    Add files via upload

---
+++ find_odd_even.py
@@ -0,0 +1,28 @@
+"""
+=================================================
+EVEN OR ODD CHECKER
+=================================================
+
+Problem Statement:
+Write a Python program to check whether a number
+is Even or Odd.
+
+-------------------------------------------------
+Input Example:
+7
+
+Output Example:
+Odd
+
+=================================================
+"""
+
+# Write your code below this line
+# Even or Odd Checker
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
