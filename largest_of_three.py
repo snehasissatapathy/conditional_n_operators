6660ea0aa1de66a376c4349f93cae21866d7ba21
Snehasis Satapathy <snehasissatapathy2005@gmail.com>
2026-05-27 16:32:39 +0530

    Add files via upload

---
+++ largest_of_three.py
@@ -0,0 +1,25 @@
+"""
+=================================================
+LARGEST OF THREE NUMBERS
+=================================================
+
+Problem Statement:
+Write a Python program to find the largest among three numbers.
+
+-------------------------------------------------
+Input Example:
+10
+45
+23
+
+Output Example:
+45 is the largest number
+
+-------------------------------------------------
+Hints:
+Use if-elif-else carefully.
+
+=================================================
+"""
+
+# Write your code below this line
